185ac9f355a68fc70494bd2c0c7a8c1b3d7c6a10
fc0382 <gregorio.fracella@allitude.it>
2026-03-04 16:07:44 +0100

    Update .gitignore and refactor git-list-repos.ps1 to use parameters for filtering and searching repositories; add utils.psm1 for utility functions.

--- git-list-repos.ps1
+++ git-list-repos.ps1
@@ -1,141 +1,29 @@
+# .\git-list-repos.ps1 -paramRepoNameFilter 'wealth-management*' -paramFilePathFilter '*.java' -paramSearchString 'TKK19R'
+param (    
+    [Parameter(Mandatory=$true, HelpMessage = "The repository name filter to use (e.g. 'wealth-management*')", Position = 0)]
+    [string]$paramRepoNameFilter,    
+    [Parameter(Mandatory=$true, HelpMessage = "The file path filter to use (e.g. '*.java')", Position = 1)]
+    [string]$paramFilePathFilter,
+    [Parameter(Mandatory=$true, HelpMessage = "The search string to use (e.g. 'TKK19R')", Position = 2)]
+    [string]$paramSearchString
+ )
+
+Import-Module -Verbose -Name (Resolve-Path  ".\config.psm1")
 Import-Module -Verbose -Name (Resolve-Path  ".\utils.psm1")
-function Read-Git-Repositories {
-    echo_ok "Starting to list repositories for organization $($orgName)..."
-    $headers = @{
-        "Authorization" = [String]::Format("Bearer {0}", $gitAccessToken)
-        "Content-Type"  = "application/json"
-    }
-    # $orgName = "corebanking"
-    # $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/$($orgName)/repos"
-    $pageNbr = 1
-    $idx = 1
-    $repos = @() # array vuoto per memorizzare i repository
-    while ($true) {
-        $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/$($orgName)/repos?sort=full_name&per_page=100&page=$($pageNbr)"
-        $githubRepositories = Invoke-RestMethod -Method get -Uri $reposAPIUri -Headers $headers 
-        $pageNbr += 1
-        foreach ($respository in $githubRepositories) {
-            $idx += 1
-            $repos += $respository
-            # echo_ok($idx.ToString() + "-" +$respository.name)
-        }
-        if ($githubRepositories.Count -eq 0) {
-            break
-        }        
-    } 
-    return $repos
-    # # # $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/corebanking/repos?page=2"
-    # # # $githubRepositories = Invoke-RestMethod -Method get -Uri $reposAPIUri -Headers $headers 
-    # # # # $githubRepositories | Out-File -FilePath "reposAPIUri.html" -Append -Encoding utf8
-    # # # # Write-Host "Output is $($githubRepositories)"
-    # # # $githubRepositories | Out-File -FilePath "reposAPIUri.html" -Append -Encoding utf8
-    # # # Write-Host "Output is $($githubRepositories)"
-}
 
-function Clone-Git-Repositories {
-    param(
-        [pscustomobject]$repoList 
-    )
-    echo_ok "Creating temporary directory for cloning repositories: $($rootCloneTempDirectory)..."
-    # TODO GREG da ripristinare - I
-    # if (!(Test-Path -Path $rootCloneTempDirectory)) {
-    #     New-Item -ItemType Directory -Path $rootCloneTempDirectory
-    # }
-    # else {
-    #     echo_error "Directory $($rootCloneTempDirectory) already exists. Deleting it..."
-    #     return     
-    # }
-    # TODO GREG da ripristinare - F
-    # $repoList = Read-Git-Repositories
-    echo_ok "Total repositories passed as params: $($repoList.Count)"
-    $idx = 0
-    foreach ($repo in $repoList) {
-        if ($repo.name.StartsWith('.')) {
-            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
-            continue
-        }        
-        else {
-            $idx += 1
-            echo_ok("[$idx/$($repoList.Count)] Cloning repository $($repo.name) from $($repo.clone_url) to $($rootCloneTempDirectory)\$($repo.name)")
-            git clone $repo.clone_url "$($rootCloneTempDirectory)\$($repo.name)"
-        }
-    }   
-}
-function Filter-Git-Repositories {
-    param(
-        [pscustomobject]$repoList 
-    )
-    echo_ok("Filtering repos...")
-    $filteredRepos = @() # array vuoto per memorizzare i match trovati
-    foreach ($repo in $repoList) {
-        if ($repo.name.StartsWith('.')) {
-            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
-            continue
-        }        
-        else {
-            if ($repo.name -like "wealth-management*") {
-                echo_ok("Repository $($repo.name) matches the filter criteria.")
-                $filteredRepos += $repo
-            }
-        }
-    }
-    return $filteredRepos
-}
-
-function Grep-Git-Repositories {
-    param(
-        [pscustomobject]$repoList ,
-        [string]$searchString
-    )
-    $matchesFound = New-Object System.Collections.Generic.List[System.String] # array vuoto per memorizzare i match trovati
-    $matchesFound.Clear()
-    foreach ($repo in $repoList) {
-        if ($repo.name.StartsWith('.')) {
-            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
-            continue
-        }        
-        else {
-            echo_ok("")            
-            echo_ok("Searching for string '$searchString' in repository $($repo.name)...")            
-            Set-Location "$($rootCloneTempDirectory)\$($repo.name)"
-            $curDir = Get-Location
-            $branches = git branch -r | Select-String -Pattern "->" -NotMatch | Select-String -pattern "^  origin/" | ForEach-Object { $_ -replace '^  origin/', '' }
-            $branchIdx = 0
-            foreach ($branch in $branches) {
-                echo_ok("")            
-                echo_ok("Searching for string '$searchString' in repository $($repo.name) and branch $branch...")            
-                echo_ok("[$($branchIdx)/$($branches.Count)] checkout of branch $branch...")
-                $res = git checkout $branch 
-                echo_ok("[$($branchIdx)/$($branches.Count)] pull of branch $branch...")
-                $res = git pull           
-                Get-ChildItem -Path $curDir -Recurse | Select-String -Pattern $searchString -CaseSensitive | ForEach-Object {
-                    $matchInfo = $_
-                    # $filePath = $matchInfo.Path
-                    $filePath = [System.IO.Path]::GetFileName($matchInfo.Path)
-                    $lineNumber = $matchInfo.LineNumber
-                    $lineText = $matchInfo.Line
-                    $matchesFound.Add("$($repo.name):$($branch) $($filePath):$($lineNumber):$($lineText)")
-                }
-                echo_ok("Searching for string '$searchString' in repository $($repo.name) and branch $branch done.")            
-                $branchIdx += 1
-            }
-        }
-    }
-    Set-Location "$($rootCloneTempDirectory)"
-    return $matchesFound
-}
-
-
-
+#######################################################
 # Main program
+#######################################################
+
 $curDir = Get-Location
 $repoList = Read-Git-Repositories
 echo_ok "Total repositories found: $($repoList.Count)"
-$repoList = Filter-Git-Repositories -repoList $repoList
+
+$repoList = Filter-Git-Repositories -repoList $repoList -repoNameFilter $paramRepoNameFilter
 echo_ok "Total repositories filtered: $($repoList.Count)"
 
 Clone-Git-Repositories -repoList $repoList
-$matchesFound = Grep-Git-Repositories -repoList $repoList -searchString "TKK18R"
+$matchesFound = Grep-Git-Repositories -repoList $repoList -searchString $paramSearchString -filePathFilter $paramFilePathFilter
 if ($matchesFound.Count -eq 0) {
     echo_ok "No matches found for the search string."
 }
---
+++ utils.psm1
@@ -0,0 +1,142 @@
+
+function echo_error {
+    Write-Host $args[0] -ForegroundColor Red
+}
+function echo_ok {
+    Write-Host $args[0] -ForegroundColor Green
+}
+function echo_ok_yellow {
+    Write-Host $args[0] -ForegroundColor Yellow -BackgroundColor Black
+}
+
+function Read-Git-Repositories {
+    echo_ok "Starting to list repositories for organization $($orgName)..."
+    $headers = @{
+        "Authorization" = [String]::Format("Bearer {0}", $gitAccessToken)
+        "Content-Type"  = "application/json"
+    }
+    # $orgName = "corebanking"
+    # $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/$($orgName)/repos"
+    $pageNbr = 1
+    $idx = 1
+    $repos = @() # array vuoto per memorizzare i repository
+    while ($true) {
+        $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/$($orgName)/repos?sort=full_name&per_page=100&page=$($pageNbr)"
+        $githubRepositories = Invoke-RestMethod -Method get -Uri $reposAPIUri -Headers $headers 
+        $pageNbr += 1
+        foreach ($respository in $githubRepositories) {
+            $idx += 1
+            $repos += $respository
+            # echo_ok($idx.ToString() + "-" +$respository.name)
+        }
+        if ($githubRepositories.Count -eq 0) {
+            break
+        }        
+    } 
+    return $repos
+    # # # $reposAPIUri = "https://github.servizi.allitude.it/api/v3/orgs/corebanking/repos?page=2"
+    # # # $githubRepositories = Invoke-RestMethod -Method get -Uri $reposAPIUri -Headers $headers 
+    # # # # $githubRepositories | Out-File -FilePath "reposAPIUri.html" -Append -Encoding utf8
+    # # # # Write-Host "Output is $($githubRepositories)"
+    # # # $githubRepositories | Out-File -FilePath "reposAPIUri.html" -Append -Encoding utf8
+    # # # Write-Host "Output is $($githubRepositories)"
+}
+
+function Clone-Git-Repositories {
+    param(
+       [Parameter(Mandatory=$true)][pscustomobject]$repoList 
+    )
+    echo_ok "Creating temporary directory for cloning repositories: $($rootCloneTempDirectory)..."
+    # Cancellazione della directory di destinazione se esiste, altrimenti creazione della directory di destinazione
+    # TODO GREG da ripristinare - I
+    # if (!(Test-Path -Path $rootCloneTempDirectory)) {
+    #     New-Item -ItemType Directory -Path $rootCloneTempDirectory
+    # }
+    # else {
+    #     echo_error "Directory $($rootCloneTempDirectory) already exists. Deleting it..."
+    #     return     
+    # }
+    # TODO GREG da ripristinare - F
+    # $repoList = Read-Git-Repositories
+    echo_ok "Total repositories passed as params: $($repoList.Count)"
+    $idx = 0
+    foreach ($repo in $repoList) {
+        if ($repo.name.StartsWith('.')) {
+            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
+            continue
+        }        
+        else {
+            $idx += 1
+            echo_ok("[$idx/$($repoList.Count)] Cloning repository $($repo.name) from $($repo.clone_url) to $($rootCloneTempDirectory)\$($repo.name)")
+            git clone $repo.clone_url "$($rootCloneTempDirectory)\$($repo.name)"
+        }
+    }   
+}
+function Filter-Git-Repositories {
+    param(
+        [Parameter(Mandatory=$true)][pscustomobject]$repoList,
+        [Parameter(Mandatory=$true)][string]$repoNameFilter 
+    )
+    echo_ok("Filtering repos...")
+    $filteredRepos = @() # array vuoto per memorizzare i match trovati
+    foreach ($repo in $repoList) {
+        if ($repo.name.StartsWith('.')) {
+            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
+            continue
+        }        
+        else {
+            if ($repo.name -like $repoNameFilter) {
+                echo_ok("Repository $($repo.name) matches the filter criteria.")
+                $filteredRepos += $repo
+            }
+        }
+    }
+    return $filteredRepos
+}
+
+function Grep-Git-Repositories {
+    param(
+        [Parameter(Mandatory=$true)] [pscustomobject]$repoList ,
+        [Parameter(Mandatory=$true)] [string]$searchString,
+        [Parameter(Mandatory=$true)] [string]$filePathFilter
+    )
+    $matchesFound = New-Object System.Collections.Generic.List[System.String] # array vuoto per memorizzare i match trovati
+    $matchesFound.Clear()
+    foreach ($repo in $repoList) {
+        if ($repo.name.StartsWith('.')) {
+            echo_ok($repo.name, "Skipping repository $($repo.name) because it starts with a dot.")
+            continue
+        }        
+        else {
+            echo_ok("")            
+            echo_ok_yellow("Searching for string '$searchString' in repository $($repo.name)...")            
+            Set-Location "$($rootCloneTempDirectory)\$($repo.name)"
+            $curDir = Get-Location
+            $branches = git branch -r | Select-String -Pattern "->" -NotMatch | Select-String -pattern "^  origin/" | ForEach-Object { $_ -replace '^  origin/', '' }
+            $branchIdx = 0
+            foreach ($branch in $branches) {
+                echo_ok("")            
+                echo_ok("Searching for string '$searchString' in repository $($repo.name) and branch $branch...")            
+                echo_ok("[$($branchIdx)/$($branches.Count)] checkout of branch $branch...")
+                $res = git checkout $branch 
+                echo_ok("[$($branchIdx)/$($branches.Count)] pull of branch $branch...")
+                $res = git pull           
+                $seachFiles = [System.IO.Path]::Combine($curDir, $filePathFilter)
+                Get-ChildItem -Path $seachFiles -Recurse | Select-String -Pattern $searchString -CaseSensitive | ForEach-Object {
+                    $matchInfo = $_
+                    # $filePath = $matchInfo.Path
+                    $filePath = [System.IO.Path]::GetFileName($matchInfo.Path)
+                    $lineNumber = $matchInfo.LineNumber
+                    $lineText = $matchInfo.Line
+                    $matchesFound.Add("$($repo.name):$($branch) $($filePath):$($lineNumber):$($lineText)")
+                }
+                echo_ok("Searching for string '$searchString' in repository $($repo.name) and branch $branch done.")            
+                $branchIdx += 1
+            }
+        }
+    }
+    Set-Location "$($rootCloneTempDirectory)"
+    return $matchesFound
+}
+
+Export-ModuleMember -Function echo_error, echo_ok, echo_ok_yellow, Read-Git-Repositories, Clone-Git-Repositories, Filter-Git-Repositories, Grep-Git-Repositories
